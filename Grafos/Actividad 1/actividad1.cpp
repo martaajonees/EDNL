4f1dbe2bb1f789ddb8309bb6a2b342adc4e5445c
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-09 18:00:11 +0200

    inicio

---
+++ Grafos/Actividad 1/actividad1.cpp
@@ -0,0 +1,44 @@
+using namespace std;
+#include <cassert>         
+#include "../Materiales Grafos/grafoPMC.h"
+
+template<typename tCoste>
+vector<tCoste> DijkstraInv(GrafoP<tCoste>& G, typename GrafoP<tCoste>::vertice destino,
+                            vector<typename GrafoP<tCoste>::vertice>& P ){
+    //Estructuras
+    const size_t n = G.numVert();
+    vector<bool> S(n, false);
+    typename GrafoP<tCoste>::vertice vertice;
+    vertice v,w;
+    vector<tCoste> Min;
+
+    //Inicializamos Min y P al reves
+    for(size_t i=0; i<n; i++){
+        Min[i]= G[i][destino]
+    }
+    Min[destino]=0;
+    P = vector<vertices>(n, destino);
+    S[destino]= true;
+
+    for(size_t i =0; i<=n-2; i++){
+
+        for(v=0; v<=n-1;v++){
+            tCoste CosteMin = GrafoP<tCoste>::INFINITO;
+            if(!S[v] && Min[v]<CosteMin){
+                CosteMin = Min[v];
+                w=v;
+            }
+        }
+        S[w]= true;
+        for(v=0; v<=n-1;v++){
+            if(!S[v]){
+                tCoste O = suma(Min[w], G[w][w]);
+                if(Min[v]>O){
+                    Min[v]= O;
+                    P[v]= w;
+                }
+            }
+        }
+    }
+
+}
