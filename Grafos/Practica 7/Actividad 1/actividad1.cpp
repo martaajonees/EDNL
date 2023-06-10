5be4d6db81a69b0dda810da6d674e552407e3ebb
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-10 12:32:32 +0200

    act1

--- Grafos/Practica 7/Actividad 1/actividad1.cpp
+++ Grafos/Practica 7/Actividad 1/actividad1.cpp
@@ -4,7 +4,7 @@ template<typename tCoste>
 struct Viaje{
     public:
         tCoste Coste;
-        typename vector<tCoste>::vertice origen, destino;
+        typename GrafoP<tCoste>::vertice origen, destino;
 };
 
 template<typename tCoste>
@@ -16,18 +16,20 @@ Viaje<tCoste> otravezungrafo(GrafoP<tCoste> G){
     matriz<vertice> Vmax(G.numVert());
     matriz<tCoste> max = FloydMax(G,Vmax); //Matriz de costes máximos
 
+    cout<<"\tFloyd"<<endl;
+    cout<<max<<endl;
+
     Viaje<tCoste> viaje;
     viaje.origen = viaje.destino = viaje.Coste = 0; //Inicializamos a 0
 
     //Calculamos el máximo de los costes entre todos los nodos
     for(vertice i = 0; i< G.numVert(); i++){
         for(vertice j = 0; j<G.numVert(); j++){
-            if(G[i][j]!= INFINITO && viaje.Coste < max[i][j]) {
+            if(max[i][j]!= INFINITO && viaje.Coste < max[i][j]) {
                 viaje.origen = i;
                 viaje.destino = j;
-                viaje.coste = max[i][j];
+                viaje.Coste = max[i][j];
             }
-
         }
     }
     return viaje;
