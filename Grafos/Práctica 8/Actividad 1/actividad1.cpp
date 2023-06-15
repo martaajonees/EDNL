8faf64e4a5f42c3f4243a59b296124b3ca2c070f
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-15 19:00:40 +0200

    act1

---
+++ Grafos/Práctica 8/Actividad 1/actividad1.cpp
@@ -0,0 +1,34 @@
+template<typename tCoste>
+struct Isla{
+    tCoste x, y;
+};
+
+template<typename tCoste>
+void tombuctu(Grafo A, vector<Isla<tCoste> > islas){
+    typedef typename GrafoP<tCoste>::vertice vertice;
+    size_t n = A.numVert();
+    //Crear grafo ponderado
+    GrafoP<double> G (n);
+    Particion p(n);
+
+    //Recorremos la matriz de adyascencia
+    for( int i = 0; i < n ; i++){
+        for(int j = 0; j < n; j++){
+            if(A[i][j]!= 0){
+                //Le asignamos distancia euclidiana
+                G[i][j] = sqrt(pow(islas[i].x - islas[i].y, 2) + pow(islas[j].x - islas[j].y, 2));
+                //Unimos las islas que estén conectadas entre ellas en el tad particion
+                if(p.encontrar(i) != p.encontrar(j)){
+                    p.unir(p.encontrar(i), p.encontrar(j));
+                }
+            }
+        }
+    }
+    //Hacemos Floys para los costes mínimos
+    vector<vertice> vert(n);
+    vector<tCoste> mCoste = Floyd(G, vert);
+
+    cout << "Costes minimos \n" << mCoste;
+
+    return p;
+}
