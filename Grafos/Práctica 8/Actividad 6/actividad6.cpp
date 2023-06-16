0e9e17c21ca3f78cf7eeb7f0a862b7e46604ffb6
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-16 14:13:54 +0200

    act6

--- Grafos/Práctica 8/Actividad 6/actividad6.cpp
+++ Grafos/Práctica 8/Actividad 6/actividad6.cpp
@@ -1,20 +1,19 @@
+//Mayor caudal posible, menor longitud
 template<typename tCoste>
 void emasajer(GrafoP<tCoste> ciudades, GrafoP<tCoste> caudales, double subvencion_canal, double coste_km){
     typedef typename GrafoP<tCoste>::vertice vertice;
     size_t n = ciudades.numVert();
     //Creo nuevo grafo
     GrafoP<tCoste> canal(n);
-    tCoste total = 0;
+    tCoste longitud = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             if(ciudades[i][j] != GrafoP<tCoste>::INFINITO && canal[i][j] != GrafoP<tCoste>::INFINITO){
                 canal[i][j] = suma(ciudades[i][j]* subvencion_canal,  - caudales[i][j]* coste_km); 
-                total += canal[i][j];
             }
         }
     }
 
     //Ya tenemos los costes, ahora los mínimos
     cout<< "Canales \n" << Kruskall(canal);
-    cout<< "Coste : " << total;
 }
