ede77da1bfd855f50e5ba43c90553132fcd19526
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-16 10:37:55 +0200

    readme

---
+++ Grafos/Práctica 8/Actividad 3/actividad3.cpp
@@ -0,0 +1,35 @@
+
+//Para encontrar un arbol de extensión máxima utilizamos Kruskal (por ejemplo)
+template<typename tCoste>
+void arbol_extension_maximo(const GrafoP<tCoste>& G){
+    assert(!G.esDirigido());
+
+   typedef typename GrafoP<tCoste>::vertice vertice;
+   typedef typename GrafoP<tCoste>::arista arista;
+   const tCoste INFINITO = GrafoP<tCoste>::INFINITO;
+   const size_t n = G.numVert();
+   GrafoP<tCoste> g(n);   // Árbol generador de coste mínimo.
+   Particion P(n);   // Partición inicial del conjunto de vértices de G.
+   Apo<arista> A(n*n);    // Aristas de G ordenadas por costes.
+
+   // Copiar aristas del grafo G en el APO A.
+   for (vertice u = 0; u < n; u++)
+      for (vertice v = u+1; v < n; v++)
+         if (G[u][v] != INFINITO)
+            A.insertar(arista(u, v, G[u][v]));
+
+   size_t i = 1;
+   while (i <= n-1) {   // Seleccionar n-1 aristas.
+      arista a = A.cima(); // arista de menor coste 
+      A.suprimir();
+      vertice u = P.encontrar(a.orig);
+      vertice v = P.encontrar(a.dest);
+      if (u != v) { // Los extremos de a pertenecen a componentes distintas
+         P.unir(u, v);
+         // Incluir la arista a en el árbol g
+         g[a.orig][a.dest] = g[a.dest][a.orig] = -1 * a.coste;
+         i++;
+      }
+   }
+   return g;
+}
