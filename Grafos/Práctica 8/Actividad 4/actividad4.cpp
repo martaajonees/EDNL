19a6cb30c3742f16ef98bfb4e4d8e8a70262bc1d
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-16 11:20:36 +0200

    readme

--- Grafos/Práctica 8/Actividad 4/actividad4.cpp
+++ Grafos/Práctica 8/Actividad 4/actividad4.cpp
@@ -1,12 +1,23 @@
 
-//Quiero todas las ciudades con todas
+//Quiero todas las ciudades con todas y longitud máxima
 //Busco el árbol de extensión máxima
 template <typename tCoste>
 void emasejer(const GrafoP<tCoste>& G){
+    typedef typename GrafoP<tCoste>::vertice vertice;
     //Creamos grafo nuevo
     GrafoP<tCoste> Max(G.numVert());
     Max = arbol_extension_maximo(G);
 
+    //Calculamos longitud
+    tCoste total = 0;
+    for(vertice i = 0; i < n ; i++){
+        for(vertice j = 0; j < n; j++){
+            if(Max[i][j] != GrafoP<tCoste>::INFINITO){
+                total += fibra[i][j];
+            }
+        }
+    }
+
     cout<< "Arbol de extension maximo\n"<< Max;
-    cout << "Longitud: " << Max.numVert(); 
+    cout << "Longitud: " << total; 
 }
