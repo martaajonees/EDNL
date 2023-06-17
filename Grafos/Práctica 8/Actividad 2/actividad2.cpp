aa05b52dde9d8c4253124dc6ad22f344f21f9112
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-17 13:30:51 +0200

    act2

--- Grafos/Práctica 8/Actividad 2/actividad2.cpp
+++ Grafos/Práctica 8/Actividad 2/actividad2.cpp
@@ -1,12 +1,4 @@
 
-#include "../Materiales Grafos/alg_grafoPMC.hpp"
-#include "../Materiales Grafos/grafoMA.h"
-#include "../Materiales Grafos/particion.hpp"
-#include <cmath>
-#include <algorithm>
-#include <iostream>
-using namespace std;
-
 template<typename tCoste>
 struct Ciudad {
     Ciudad(tCoste x, tCoste y): x(x), y(y) {}
@@ -38,20 +30,26 @@ void tombuctu2(Grafo A, vector<Ciudad<tCoste> > ciudades){
     }
     //Necesito n* (n-1) / 2
 
-    matriz<LineaAerea<tCoste> > vuelos;
+    matriz<LineaAerea<tCoste> > vuelos(n);
+    map<int, int > repre;
+    int isla = 0; //Empezamos mapeando por 0
+    for(int i = 0; i < numIslas; i++){ //Mapeo representantes
+        if(repre.insert(make_pair(p.encontrar(i), isla)).second) isla++;
+    }
 
     //Buscamos la mejor línea entre cada par de islas
     for(int i = 0; i < n-1; i++){
         for(int j = i+1; j < n; j++){
-            int isla1 = p.encontrar(i);
-            int isla2 = p.encontrar(j);
+            int isla1 = repre[p.encontrar(i)];
+            int isla2 = repre[p.encontrar(j)];
             if(isla1 != isla2){ //Para distintas islas creamos vuelo
-                tCoste distancia = sqrt(pow(ciudades[i].x- ciudades[j].y, 2) + pow(ciudades[j].x - ciudades[j].y, 2));
+                tCoste distancia = sqrt(pow(ciudades[i].x- ciudades[j].x, 2) + pow(ciudades[i].y - ciudades[j].y, 2));
                 if(distancia < vuelos[isla1][isla2].distancia) //Comparamos si la distancia entre estas dos islas es mínima
                     vuelos[isla1][isla2] = vuelos[isla2][isla1] = LineaAerea<tCoste>(i, j, distancia);
             }
         }
     }
+
 
     //Cojo de la matriz los vuelos y omito los valores nulos
     vector<LineaAerea<tCoste> >lineas(numIslas);
