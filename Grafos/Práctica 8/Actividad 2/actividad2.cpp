5010cf96a306423123add05120bb9cd4ec3902e8
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-06-19 16:26:39 +0200

    act2

--- Grafos/Práctica 8/Actividad 2/actividad2.cpp
+++ Grafos/Práctica 8/Actividad 2/actividad2.cpp
@@ -1,67 +1,80 @@
 
-template<typename tCoste>
+struct LineaAerea {
+    LineaAerea(Ciudad x, Ciudad y, double coste): a{x}, b{y}, distancia{coste}{}
+    Ciudad a, b;
+    double distancia;
+};
 struct Ciudad {
-    Ciudad(tCoste x, tCoste y): x(x), y(y) {}
-    tCoste x, y;
+    int x,y;
 };
 
 template<typename tCoste>
-struct LineaAerea{
-    LineaAerea(int x, int y, int distancia): x(x), y(y), distancia(distancia) {}
-    int y, x;
-    tCoste distancia;
-};
-
-//Encontrar las líneas aéreas necesarias
-template<typename tCoste>
-void tombuctu2(Grafo A, vector<Ciudad<tCoste> > ciudades){
-    size_t n = A.numVert();
-    Particion p(n);
-    size_t numIslas = n;
-
-    //Agrupo las ciudades
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(A[i][j] && p.encontrar(i) != p.encontrar(j)){
-                numIslas --;
-                p.unir(p.encontrar(i), p.encontrar(j));
-            }
-        }
-    }
-    //Necesito n* (n-1) / 2
-
-    matriz<LineaAerea<tCoste> > vuelos(n);
-    map<int, int > repre;
-    int isla = 0; //Empezamos mapeando por 0
-    for(int i = 0; i < numIslas; i++){ //Mapeo representantes
-        if(repre.insert(make_pair(p.encontrar(i), isla)).second) isla++;
-    }
-
-    //Buscamos la mejor línea entre cada par de islas
-    for(int i = 0; i < n-1; i++){
-        for(int j = i+1; j < n; j++){
-            int isla1 = repre[p.encontrar(i)];
-            int isla2 = repre[p.encontrar(j)];
-            if(isla1 != isla2){ //Para distintas islas creamos vuelo
-                tCoste distancia = sqrt(pow(ciudades[i].x- ciudades[j].x, 2) + pow(ciudades[i].y - ciudades[j].y, 2));
-                if(distancia < vuelos[isla1][isla2].distancia) //Comparamos si la distancia entre estas dos islas es mínima
-                    vuelos[isla1][isla2] = vuelos[isla2][isla1] = LineaAerea<tCoste>(i, j, distancia);
+void tombuctu2(Grafo A, vector<Ciudad> ciudades){
+    typedef typename GrafoP<tCoste>::vertice vertice;
+    //Creamos un tad particion
+    Particion p;
+    // Nº de islas
+    size_t N = ciudades.size();
+    //Recorremos la matriz de adyascencia
+    for(int i = 0; i < ciudades.size(); i++){
+        for(int j = 0; j < ciudades.size(); j++){
+            if(A[i][j]){ //Si es adyascente, hay camino directo: Misma Isla
+                if(p.encontrar(i) != p.encontrar(j)){ //Misma isla
+                    p.unir(p.encontrar(i), p.encontrar(j));
+                    N--; //Para calcular el nº de islas
+                }
             }
         }
     }
 
+    //Vemos lineas aereas más cortas entre islas y la ponemos en matriz
+    vector<LineaAerea> lineas;
+    matriz<bool> adecuada(N*N, false);
+    Apo<LineaAerea> arbol(N * (N - 1) / 2);
+    
+    //Vector de representantes
+    vector<int> representantes(N, -1);
 
-    //Cojo de la matriz los vuelos y omito los valores nulos
-    vector<LineaAerea<tCoste> >lineas(numIslas);
+    //Metemos todos los representantes
+    for(int i = 0; i < N; i++){
+        int rep = p.encontrar(i);
+        if(rep == -1){
+            representantes[rep] = i;
+        }
+    }
 
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0; j < n; j++){
-            if(vuelos[i][j] != 0){
-                lineas.push_back(vuelos[i][j]);
+    //Vemos las líneas que se puedes crear y lo metemos en el apo
+    for(vertice i = 0; i <= N-1; i++){
+        for(vertice j = 0; j <= N-1; j++){
+            int isla1 = p.encontrar(i);
+            int isla2 = p.encontrar(j);
+            if(isla1 != isla2){ //Distintas islas, necesitamos construir linea
+                double dist = distancia(ciudades[i], ciudades[j]);
+                LineaAerea linea(ciudades[i], ciudades[j], dist);
+                arbol.insertar(linea); //Inserto línea en el apo
             }
         }
     }
 
-    return lineas
+    while(lineas.size() < (N * (N - 1) / 2)){ //Acabamos cuando tengamos las N * (N-1) / 2 ciudades
+        LineaAerea linea = arbol.cima(); //El elemento en la cima es la de la distancia más corta
+        arbol.suprimir(); //Elimino el apo en la cima
+
+        int isla1 = p.encontrar(representantes[linea.a]); //Origen de la cima
+        int isla2 = p.encontrar(representantes[linea.b]); //Destino
+
+        //Vamos a la casilla de la matriz de booleanos
+        if(!adecuada[isla1][isla2]){ //Si es true descartamos la cima, si es false
+            lineas.push_back(linea); //Metemos la linea en el vector resultado
+            adecuada[isla1][isla2] = true;
+        }
+    }
+
+    return lineas;
 
 }
+
+double distancia(Ciudad a, Ciudad b){
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
