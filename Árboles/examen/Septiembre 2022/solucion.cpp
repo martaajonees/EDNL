5c422104a33fd9a949571592a0acabc1be69b244
Marta Jones <100365874+martaajonees@users.noreply.github.com>
2023-09-10 18:48:03 +0200

    Update solucion.cpp

--- Árboles/examen/Septiembre 2022/solucion.cpp
+++ Árboles/examen/Septiembre 2022/solucion.cpp
@@ -12,13 +12,23 @@ int desequilibrio(const Abin<T>& A);
 template<typename T>
 int REC_desequilibrio(const Abin<T>& A, typename Abin<T>::nodo n);
 
+template<typename T>
+bool orden(const Abin<T>& A);
+
+template<typename T>
+bool recOrden(const Abin<T>& A, typename Abin<T>::nodo n);
+
 
 //Funciones
 
 template<typename T>
 bool es_avl (const Abin<T>& A){
-    if(desequilibrio(A) == 0 || desequilibrio(A) == 1){
-        return true;
+    if(desequilibrio(A) == 0 || desequilibrio(A) == 1){ // Está equilibrado
+        if(orden(A)){
+            return true;
+        } else {
+            return false;
+        }
     }
     else return false;
 }
@@ -44,5 +54,32 @@ int alturaRec(const Abin<T>& A, typename Abin<T>::nodo n) {
         return 0;
     } else {
         return 1 + max(alturaRec(A, A.hijoDrcho(n)), alturaRec(A, A.hijoIzqdo(n)));
+    }
+}
+
+//COmprobar que se cumple la condición del orden
+template<typename T>
+bool orden(const Abin<T>& A){
+    return recOrden(A, A.raiz());
+}
+
+template<typename T>
+bool recOrden(const Abin<T>& A, typename Abin<T>::nodo n){
+    if(n == Abin<T>::NODO_NULO){
+        return true; // Llego al final
+    } else {
+        if(A.hijoDrcho(n) != Abin<T>::NODO_NULO){
+            if(A.elemento(A.hijoDrcho(n)) <= A.elemento(n)){
+                return false;
+            }
+        }
+        if(A.hijoIzqdo(n) != Abin<T>::NODO_NULO){
+            if(A.elemento(A.hijoIzqdo(n)) >= A.elemento(n)){
+                return false;
+            }
+        }
+
+        return recOrden(A, A.hijoDrcho(n)) && recOrden(A, A.hijoIzqdo(n));
+        
     }
 }
